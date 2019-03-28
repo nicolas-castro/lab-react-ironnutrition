537bce53a65abb807769562f3cc7dc9a723fa53e
Nicolas Castro <nicolascastro@me.com>
2019-03-27 21:22:13 -0400

    Added All Food

--- starter-code/src/FoodBox.js
+++ starter-code/src/FoodBox.js
@@ -10,39 +10,41 @@ class FoodBox extends Component {
     const {foods} = this.state
 
     return (
-      <div className="box">
-        <article className="media">
-          <div className="media-left">
-            <figure className="image is-64x64">
-              <img src="https://i.imgur.com/eTmWoAN.png" />
-            </figure>
-          </div>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>Pizza</strong> <br />
-                <small>400 cal</small>
-              </p>
-            </div>
-          </div>
-          <div className="media-right">
-            <div className="field has-addons">
-              <div className="control">
-                <input
-                  className="input"
-                  type="number" 
-                  value="1"
-                />
+      foods.map( (currentFood, index)=>    
+          <div className="box" key={index} currentFood={currentFood} index={index} >
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-64x64">
+                  <img src={currentFood.image} />
+                </figure>
               </div>
-              <div className="control">
-                <button className="button is-info">
-                  +
-                </button>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>{currentFood.name}</strong> <br />
+                    <small>{currentFood.calories} cal</small>
+                  </p>
+                </div>
               </div>
-            </div>
+              <div className="media-right">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="number" 
+                      value="1"
+                    />
+                  </div>
+                  <div className="control">
+                    <button className="button is-info">
+                      +
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </article>
           </div>
-        </article>
-      </div>
+      )
     );
   }
 
