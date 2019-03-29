ece4df799dd860f313747ef1f84f42de61cb4bcc
Nicolas Castro <nicolascastro@me.com>
2019-03-29 19:35:39 -0400

    added styling

--- starter-code/src/FoodBox.js
+++ starter-code/src/FoodBox.js
@@ -11,38 +11,40 @@ class FoodBox extends Component {
 
     return (
       foods.map( (currentFood, index)=>    
-          <div className="box" key={index} currentFood={currentFood} index={index} >
-            <article className="media">
-              <div className="media-left">
-                <figure className="image is-64x64">
-                  <img src={currentFood.image} />
-                </figure>
-              </div>
-              <div className="media-content">
-                <div className="content">
-                  <p>
-                    <strong>{currentFood.name}</strong> <br />
-                    <small>{currentFood.calories} cal</small>
-                  </p>
+          <div className="column is-one-third">
+            <div className="box" key={index} currentFood={currentFood} index={index} >
+              <article className="media is-half">
+                <div className="media-left">
+                  <figure className="image is-64x64">
+                    <img src={currentFood.image} />
+                  </figure>
                 </div>
-              </div>
-              <div className="media-right">
-                <div className="field has-addons">
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="number" 
-                      value="1"
-                    />
-                  </div>
-                  <div className="control">
-                    <button className="button is-info">
-                      +
-                    </button>
+                <div className="media-content">
+                  <div className="content">
+                    <p>
+                      <strong>{currentFood.name}</strong> <br />
+                      <small>{currentFood.calories} cal</small>
+                    </p>
                   </div>
                 </div>
-              </div>
-            </article>
+                <div className="media-right">
+                  <div className="field has-addons">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="number" 
+                        value="1"
+                      />
+                    </div>
+                    <div className="control">
+                      <button className="button is-info">
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
           </div>
       )
     );
